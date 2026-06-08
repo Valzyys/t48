bb8183584a592eb0f53d1021b8bc38556b43c93c
Valzz <jkt48connect@gmail.com>
2026-06-08 15:18:47 +0700

    Update features-1.tsx

--- src/sections/features-1.tsx
+++ src/sections/features-1.tsx
@@ -2,7 +2,6 @@
 
 import Badge from "@/components/badge"
 import Card from "@/components/card"
-import Carousel from "@/components/carousel"
 import SlideEffect from "@/components/slide-effect"
 import Image from "next/image"
 
@@ -16,14 +15,6 @@ const settings = {
   card_1: {
     title: 'Akses Semua Show',
     content: 'Dengan membership bulanan, kamu bisa nonton semua show theater JKT48 tanpa batas. Dari setlist reguler hingga show spesial, semua tersedia di satu tempat.',
-    carousel_images: [
-      'https://upload.wikimedia.org/wikipedia/id/thumb/6/6a/JKT48_Logo.svg/1200px-JKT48_Logo.svg.png',
-      'https://cdn.worldvectorlogo.com/logos/react-2.svg',
-      'https://cdn.worldvectorlogo.com/logos/vue-9.svg',
-      'https://cdn.worldvectorlogo.com/logos/angular-icon-1.svg',
-      'https://cdn.worldvectorlogo.com/logos/nestjs.svg',
-      'https://cdn.worldvectorlogo.com/logos/laravel-2.svg',
-    ]
   },
   card_2: {
     title: 'Harga Per Show Murah',
@@ -43,31 +34,24 @@ const settings = {
 export default function Features1() {
   return (
     <div id='features' className="space-y-6 sm:space-y-7 md:space-y-8 lg:space-y-10 mx-auto text-center">
-      {/* Badge */}
       <SlideEffect>
         <Badge number={settings.badge.number} text={settings.badge.text} />
       </SlideEffect>
 
-      {/* Title */}
       <SlideEffect>
         <h2 className="text-2xl md:text-4xl lg:text-header capitalize font-medium leading-none text-transparent bg-clip-text bg-gradient-to-b from-black to-black/60">{settings.title}</h2>
       </SlideEffect>
 
-      {/* Description */}
       <SlideEffect className="px-2 sm:px-10 md:px-0 w-full md:max-w-3/4 mx-auto text-sm lg:text-base">{settings.description}</SlideEffect>
 
-      {/* Cards */}
       <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
-        {/* card 1 */}
         <SlideEffect direction="right" className="col-span-1 lg:col-span-3 h-full" isSpring={false}>
           <Card>
             <h3 className="text-xl md:text-title text-black font-medium">{settings.card_1.title}</h3>
-            <p className="mb-4">{settings.card_1.content}</p>
-            <Carousel images={settings.card_1.carousel_images} />
+            <p>{settings.card_1.content}</p>
           </Card>
         </SlideEffect>
 
-        {/* card 2 */}
         <SlideEffect direction="left" duration={1.3} className="col-span-1 lg:col-span-2 h-full" isSpring={false}>
           <Card>
             <h3 className="text-xl md:text-title text-black font-medium">{settings.card_2.title}</h3>
@@ -75,7 +59,6 @@ export default function Features1() {
           </Card>
         </SlideEffect>
 
-        {/* card 3 */}
         <SlideEffect direction="right" duration={1} className="col-span-1 lg:col-span-2 h-full" isSpring={false}>
           <Card>
             <h3 className="text-xl md:text-title text-black font-medium">{settings.card_3.title}</h3>
@@ -83,14 +66,12 @@ export default function Features1() {
           </Card>
         </SlideEffect>
 
-        {/* card 4 */}
         <SlideEffect direction="left" className="col-span-1 lg:col-span-3 h-full" isSpring={false}>
           <Card className="flex flex-col lg:flex-row justify-center items-center">
             <div className="space-y-3 md:space-y-5">
               <h3 className="text-xl md:text-title text-black font-medium">{settings.card_4.title}</h3>
               <p>{settings.card_4.content}</p>
             </div>
-
             <Image className="w-32 my-auto mx-auto" src={settings.card_4.image} alt={settings.card_4.title} width={512} height={512} />
           </Card>
         </SlideEffect>
